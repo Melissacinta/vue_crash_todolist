8c3797ff1f3dbc479b63eec06203a2fe0cd0f517
Aguluka <melissacinta@gmail.com>
2020-10-26 18:35:04 +0100

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>I have a lot to say but just have this for now "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis obcaecati a cumque perferendis facilis cupiditate, deleniti reiciendis. Ad veritatis qui molestias, soluta earum doloremque unde id aperiam quod nesciunt amet."</p>
+  </div>
+</template>
